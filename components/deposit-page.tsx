f8018b6aee18d155b5bfdc98ec160ef4923cc605
eyesseekreality0 <eyesseekreality0@gmail.com>
2025-10-19 01:55:22 -0600

    Update deposit-page.tsx

--- components/deposit-page.tsx
+++ components/deposit-page.tsx
@@ -14,6 +14,7 @@ export function DepositPage() {
   const [isLoading, setIsLoading] = useState(false)
 
   const handleHelioDeposit = () => setShowDepositWidget(true)
+  
   const handleLinkDeposit = () => {
     setIsLoading(true)
     window.open("https://sirenspay.vercel.app/api/deposit.js", "_blank")
